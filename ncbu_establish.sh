91c54e64b5e297e628dd441efd9421b8d3f61215
clewsy <jadon@clews.pro>
2020-01-17 10:46:32 +1100

    Added comments, fixed typos.

--- ncbu_establish.sh
+++ ncbu_establish.sh
@@ -2,13 +2,22 @@
 
 ## An example docker run usage for creating an ncbu container.
 
-docker run -ti \
-	-e NEXTCLOUD_CONTAINER=nextcloud-app \			#environment variable - define the nextcloud app name.
-	-e NEXTCLOUD_DATABASE_CONTAINER=nextcloud-db \		#environment variable - define the nextcloud database name.
-	-e NEXTCLOUD_BACKUP_CRON="0 0 * * *" \			#environment variable - define the cronjob interval for running the bckup script.
-	-v /etc/localtime:/etc/localtime:ro \			#volume - link the local time to the ncbu container so the cron job runs when expected.
-	-v /var/run/docker.sock:/var/run/docker.sock:ro \	#volume - link the docker.sock to enable docker commands to reach other containers.
-	-v nextcloud-app:/mnt/nextcloud-app \			#volume - define the nextcloud app volume for mounting within the ncbu container.
-	-v nextcloud-db:/mnt/nextcloud-db \			#volume - define the nextcloud database volume for mounting within the ncbu container.
-	-v ./nextcloud-bu:/backup \				#volume - define the user-accessible volume to which the physical backups will be stored.
-	clewsy/ncbu
+##    --name		: Define a name for the container.
+## -t --tty		: Allocate a pseudo-tty.
+## -i --interactive	: Keep stdin open even if not attached.
+## -e --env		: Set an environment variable.
+## -v --volume		: Define a volume (map internal to external directory).
+
+docker run \
+	--tty 
+	--interactive \
+	--name=nextcloud-bu \
+	-env NEXTCLOUD_CONTAINER=nextcloud-app \		## Define the nextcloud app container name.
+	-env NEXTCLOUD_DATABASE_CONTAINER=nextcloud-db \	## Define the nextcloud database container name.
+	-env NEXTCLOUD_BACKUP_CRON="0 0 * * *" \		## Define the cronjob interval for running the backup script.
+	-volume /etc/localtime:/etc/localtime:ro \		## Link the local time to the ncbu container so the cron job runs when expected.
+	-volume /var/run/docker.sock:/var/run/docker.sock:ro \	## Link the docker.sock to enable docker commands to reach other containers.
+	-volume nextcloud-app:/mnt/nextcloud-app \		## Define the nextcloud app volume for mounting within the ncbu container.
+	-volume nextcloud-db:/mnt/nextcloud-db \		## Define the nextcloud database volume for mounting within the ncbu container.
+	-volume ./nextcloud-bu:/backup \			## Define the user-accessible volume to which the physical backups will be stored.
+	clewsy/ncbu						## The image to be used.
